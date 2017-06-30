062994a41c019b127aeef30305a2e20bbef96bad
Nate Geslin <ngeslin@nerdery.com>
2017-06-30 08:28:29 -0500

    renames HttpService to GithubIssueRepository

--- lib/gbre/github_issue_repository.rb
+++ lib/gbre/github_issue_repository.rb
@@ -4,12 +4,11 @@ require 'net/https'
 require 'open-uri'
 
 module Gbre
-  class HttpService
+  class GithubIssueRepository
     def self.get(uri)
       uri = URI.parse(uri)
       http = Net::HTTP.new(uri.host, 443)
       http.use_ssl = true
-
       request = Net::HTTP::Get.new(uri.path)
       request['User-Agent'] = 'n8rzz'
       response = http.request(request)

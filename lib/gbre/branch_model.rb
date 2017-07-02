4a69ce6befb2567c71ad2358696456af72fb2e44
Nate Geslin <ngeslin@nerdery.com>
2017-07-01 22:39:44 -0500

    adds basic tests to branch_model * adds editorconfig * adds rubocop * splits up logic blocks in branch_model to smaller methods

--- lib/gbre/branch_model.rb
+++ lib/gbre/branch_model.rb
@@ -6,7 +6,6 @@ require "json"
 module Gbre
   class BranchModel
     attr_accessor :name
-    attr_accessor :api_url
     attr_accessor :issue_number
     attr_accessor :issue_state
     attr_accessor :issue_title
@@ -14,29 +13,32 @@ module Gbre
     attr_accessor :is_issue
 
     def initialize(name, is_active_branch = false)
-      api_issue_url_partial = 'https://api.github.com/repos/openscope/openscope/issues'
-
       @name = name
-      @api_url = ''
       @is_active_branch = is_active_branch
-      @is_issue = name.include?('.') || name.include?('/')
+      @is_issue = name.include?('/')
+
+      set_issue_number
+    end
+
+    def set_issue_number
+      branch_name_after_slash = @name.partition('/').last
 
       return if !@is_issue
-      return if name.partition('/').last.to_i == 0
+      return if branch_name_after_slash.to_i == 0
+      return if branch_name_after_slash.include?('.');
 
-      @issue_number = name.partition('/').last.to_i
-      @api_url = "#{api_issue_url_partial}/#{@issue_number}"
+      @issue_number = branch_name_after_slash.to_i
 
       get_current_issue_status
     end
 
     def display_branch
-      colored_active_branch = @is_active_branch ? Rainbow("#{@name}").magenta : "#{@name}"
-      colored_issue_title = @is_active_branch ? Rainbow("#{@issue_title}").magenta : "#{@issue_title}"
+      colored_active_branch = build_magenta_text_with_condition(@name, @is_active_branch)
+      colored_issue_title = build_magenta_text_with_condition(@issue_title, @is_active_branch)
       colored_issue_state = ""
 
       if @issue_state != ""
-        colored_issue_state = @issue_state == 'open' ? Rainbow("(#{@issue_state})").green : Rainbow("(#{@issue_state})").red
+        colored_issue_state = build_colored_issue_state
       end
 
       if @is_issue
@@ -52,12 +54,27 @@ module Gbre
     private
 
     def get_current_issue_status
-      issue_response = JSON.parse(GithubIssueRepository.get(@api_url))
+      api_url = build_api_url
+      issue_response = JSON.parse(GithubIssueRepository.get(api_url))
 
       @issue_state = issue_response["state"]
       @issue_title = issue_response["title"]
-      # @issue_state = 'open'
-      # @issue_title = 'a placeholder issue title'
+    end
+
+    def build_api_url
+      "https://api.github.com/repos/openscope/openscope/issues/#{@issue_number}"
+    end
+
+    def build_magenta_text_with_condition(text, condition)
+      if (condition)
+        return Rainbow("#{text}").magenta
+      end
+
+      Rainbow("#{text}")
+    end
+
+    def build_colored_issue_state
+      @issue_state == 'open' ? Rainbow("#{@issue_state}").green : Rainbow("#{@issue_state}").red
     end
   end
 end
--- spec/branch_model_spec.rb
+++ spec/branch_model_spec.rb
@@ -1,28 +1,38 @@
 require "spec_helper"
+require "json"
 
 RSpec.describe Gbre::BranchModel do
-  it "should accept a name" do
-    @branchModel = Gbre::BranchModel.new('feature/123')
-    expect(@branchModel.name).to eq "feature/123"
+  let(:feature_mock) { 'feature/123' }
+  let(:develop_mock) { 'develop' }
+  let(:release_mock) { 'release/1.2.3' }
+  let(:named_branch_mock) { 'feature/threeve' }
+
+  describe "a branch that maps to an issue" do
+    before :each do
+      issue_response = {'title' => 'An Issue Title', 'status' => 'open'}.to_json
+      allow(Gbre::GithubIssueRepository).to receive(:get).and_return(issue_response)
+    end
+
+    it "should accept a name" do
+      @branchModel = Gbre::BranchModel.new(feature_mock)
+      expect(@branchModel.name).to eq feature_mock
+    end
+
+    it "sets #is_active_branch to false when no value is provided" do
+      @branchModel = Gbre::BranchModel.new(feature_mock)
+      expect(@branchModel.is_active_branch).to eq false
+    end
+
+    it "builds the correct array when #issue_state is defined" do
+      @branchModel = Gbre::BranchModel.new(release_mock)
+      expect(@branchModel.display_branch).to include("release/1.2.3", "")
+    end
   end
 
-  it "sets #is_active_branch to false when no value is provided" do
-    @branchModel = Gbre::BranchModel.new('feature/123')
-    expect(@branchModel.is_active_branch).to eq false
+  describe "a branch that does not map to an issue" do
+    before :each do
+      issue_response = {'title' => 'An Issue Title', 'status' => 'open'}.to_json
+      allow(Gbre::GithubIssueRepository).to receive(:get).and_return(issue_response)
+    end
   end
-
-  it "does not set #api_url when #is_issue is false" do
-    @branchModel = Gbre::BranchModel.new('develop')
-    expect(@branchModel.api_url).to eq ''
-  end
-
-  it "does not set #api_url when a branch name does not include a number" do
-    @branchModel = Gbre::BranchModel.new('release/0.0.0')
-    expect(@branchModel.api_url).to eq ''
-  end
-
-  # it "builds the correct array when #issue_state is defined" do
-  #   @branchModel = Gbre::BranchModel.new('release/0.0.0')
-  #
-  # end
 end
